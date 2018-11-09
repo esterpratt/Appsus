3bd3d21897479677a41a709b74dd137d25035692
Anat <Anat.md@gmail.com>
2018-11-09 09:52:18 +0200

    misterEmail: nav filter, read unread

--- js/cmps/mister-email/filter-tab.cmp.js
+++ js/cmps/mister-email/filter-tab.cmp.js
@@ -2,8 +2,21 @@
 
 export default {
     template: `
-    <section>
-        <p>filter nav </p>
+    <section class="flex">
+         <input @click="sendNavFilter" class="inline" type="radio" v-model="navValue" value="all">All
+         <input @click="sendNavFilter" class="inline" type="radio" v-model="navValue" value="read">Read
+         <input @click="sendNavFilter" class="inline" type="radio" v-model="navValue" value="unread">Unread
 </section>
-    `
+    `,
+    data() {
+        return {
+            navValue: 'all',
+        }
+    },
+    methods: {
+        sendNavFilter() {
+            this.$emit('setNavFilter', this.navValue);
+            console.log ('setnavfilter working and sending ' , this.navValue)
+        }
+    }
 }
